b0fdbc893579f9a83c5d30c537b3369c9c806b09
Igor Ferreira da Silva <igor_ferreira_silva1@hotmail.com>
2021-01-08 11:01:47 -0300

    Realizado a instalação do bootstrap, conexão com banco de dados e organização de arquivos e pastas no projeto

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
 //view engine
 app.set('view engine', 'ejs');
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//static
+app.use(express.static('public'));
+
+//database
+connection.authenticate().then(()=>{
+    console.log("Conexão com banco de dados realizada!");
+}).catch((error)=>{
+    console.log(error);
+})
+
 //routes
 app.get("/", (req,res)=>{
-    res.send("Bem-vindo ao site");
+    res.render("index");
 });
 
 
